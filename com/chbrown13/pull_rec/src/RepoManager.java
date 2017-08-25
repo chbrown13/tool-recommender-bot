1ee0eecccd3747fb3ab73a61f89c6c21e9e410d1
chbrown13 <dcbrown06@gmail.com>
2017-08-25 16:56:16 -0400

    Adding java files for Github API and Error Prone

---
+++ com/chbrown13/pull_rec/src/RepoManager.java
@@ -0,0 +1,70 @@
+package com.chbrown13.pull_rec.src;
+
+import java.io.*;
+import java.util.*;
+import com.jcabi.github.*;
+import java.util.concurrent.TimeUnit;
+import javax.json.JsonObject;
+			
+public class RepoManager {
+
+	private Repo repo;
+	private String project;
+
+	public RepoManager(Repo repo, String name) {
+		this.repo = repo;
+		this.project = name;
+	}
+	
+	private void analyze(Pull.Smart pull) {
+		try {
+			Iterator<JsonObject> fileit = pull.files().iterator();
+			while (fileit.hasNext()) {
+				JsonObject file = fileit.next();
+				Analyzer err = new Analyzer();
+				Analyzer.getFile(file.getString("raw_url"));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private ArrayList<Pull.Smart> getRequests() {
+		System.out.println("Getting pull requests...");
+		ArrayList<Pull.Smart> requests = new ArrayList<Pull.Smart>();
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("status","open");
+		Iterator<Pull> pullit = this.repo.pulls().iterate(params).iterator();
+		while (pullit.hasNext()) {
+			Pull.Smart pull = new Pull.Smart(pullit.next());
+			try {
+				/*if (new Date().getTime() - pull.createdAt().getTime() <= TimeUnit.MILLISECONDS.convert(15, TimeUnit.MINUTES)) {
+					requests.add(pull);
+					System.out.println(pull.url());
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				return null;
+			}*/
+			if (pull.number() >= 23) {
+				requests.add(pull);
+				System.out.println(pull.url());
+			}
+			} catch (IOException e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+		return requests;
+	}
+
+	public static void main(String[] args) {
+        RtGithub github = new RtGithub("<username>","<password>"); //git credentials
+        Repo repo = github.repos().get(new Coordinates.Simple("chbrown13", "RecommenderTest"));
+		RepoManager manager = new RepoManager(repo, "RecommenderTest");
+		ArrayList<Pull.Smart> requests = manager.getRequests();
+		for (Pull.Smart pull: requests) {
+			manager.analyze(pull);
+		}
+    }
+}
