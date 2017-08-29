0a17c40a14160551fc2fd444e8c898eb791827a0
chbrown13 <dcbrown06@gmail.com>
2017-08-29 16:26:37 -0400

    Parsing error_prone output in Java so no longer need Python

--- com/chbrown13/pull_rec/Analyzer.java
+++ com/chbrown13/pull_rec/Analyzer.java
@@ -1,11 +1,47 @@
 package com.chbrown13.pull_rec;
 
-import java.io.*;
-import java.net.*;
 import com.google.errorprone.*;
 import com.sun.tools.javac.main.Main.Result;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.json.JsonObject;
 
 public class Analyzer {
+
+	public static JsonObject parseErrorProne(String file) {
+        /*File f = new File(file);
+        if (!f.exists()) {
+            System.err.println("file path not specified");
+        }
+        try {
+            //String regex = "^\w+.java\:\d+\:";
+
+            Scanner sc = new Scanner(f);
+
+            while (sc.hasNextLine()) {
+                System.out.println("Hello");
+                String line = sc.nextLine();
+                if (line != null) {
+                    //if (matcher.lookingAt(regex)) {
+                        System.out.println(line);
+
+                    //}
+                }
+            }   
+			sc.close();
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }*/
+		String regex = "^[\\w+/]*\\w.java\\:\\d+\\:.*\\:.*";
+		String[] lines = file.split("\n");
+		for(String line: lines) {
+			if (line.matches(regex)) {
+				System.out.println(line);
+			}
+		}
+		return null;
+	}
 
 	public static void wgetFile(String fileUrl, String output) {
 		String s;
