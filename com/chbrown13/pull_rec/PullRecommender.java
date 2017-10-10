20235721f67f342fcc5f79503032527becaa7b63
Chris <dcbrow10@ncsu.edu>
2017-10-10 14:49:07 -0400

    Updating parameter name

--- com/chbrown13/pull_rec/PullRecommender.java
+++ com/chbrown13/pull_rec/PullRecommender.java
@@ -96,7 +96,7 @@ public class PullRecommender {
 		while (pullit.hasNext()) {
 			Pull.Smart pull = new Pull.Smart(pullit.next());
 			try {
-				if (new Date().getTime() - pull.createdAt().getTime() <= TimeUnit.MILLISECONDS.convert(15, TimeUnit.MINUTES)) {
+				if (pull.number() >= 40) {//new Date().getTime() - pull.createdAt().getTime() <= TimeUnit.MILLISECONDS.convert(15, TimeUnit.MINUTES)) {
 					requests.add(pull);
 					System.out.println("Pull Request #" + Integer.toString(pull.number()) + ": " + pull.title());
 				}
