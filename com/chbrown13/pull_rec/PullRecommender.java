da7320be9a9d22c88311d10206ac3ed4baaf3124
Chris <dcbrow10@ncsu.edu>
2017-10-10 14:51:54 -0400

    Fixed typo

--- com/chbrown13/pull_rec/PullRecommender.java
+++ com/chbrown13/pull_rec/PullRecommender.java
@@ -105,7 +105,7 @@ public class PullRecommender {
 		System.out.println("Getting new pull requests...");
 		ArrayList<Pull.Smart> requests = new ArrayList<Pull.Smart>();
 		Map<String, String> params = new HashMap<String, String>();
-		params.put("status", "all");
+		params.put("state", "all");
 		Iterator<Pull> pullit = this.repo.pulls().iterate(params).iterator();
 		while (pullit.hasNext()) {
 			Pull.Smart pull = new Pull.Smart(pullit.next());
