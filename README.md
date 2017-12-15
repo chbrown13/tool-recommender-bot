e1911b9ad1ae7ddc8a438945340271f42732cfee
Chris Brown <chbrown13>
2017-12-15 16:14:31 -0500

    First commit of searching script

--- README.md
+++ README.md
@@ -13,9 +13,10 @@ Set Up:
 	* slf4j.jar (optional)
 * Create a .github.creds file with two lines, one that contains your github username and one with your password.
 
+This branch just shows our approach for searching for Github repositories.
 
 Run from source code:
 ```
-$ sudo javac -cp jcabi-github-0.23-jar-with-dependencies.jar:error_prone_ant-2.1.0.jar:gumtree.jar:org.eclipse.jgit-4.9.0.201710071750-r.jar:jsch-0.1.46.jar:commons-email-1.3.1.jar:mail-1.4.7.jar com/chbrown13/pull_rec/ErrorProne.java com/chbrown13/pull_rec/Tool.java com/chbrown13/pull_rec/Error.java com/chbrown13/pull_rec/PullRecommender.java com/chbrown13/pull_rec/Utils.java com/chbrown13/pull_rec/Comment.java
-$ java -cp .:jcabi-github-0.23-jar-with-dependencies.jar:error_prone_ant-2.1.0.jar:gumtree.jar:org.eclipse.jgit-4.9.0.201710071750-r.jar:jsch-0.1.46.jar:commons-email-1.3.1.jar:mail-1.4.7.jar com.chbrown13.pull_rec.PullRecommender <owner> <project>
+$ sudo javac -cp jcabi-github-0.23-jar-with-dependencies.jar com/chbrown13/pull_rec/PullRecommender.java
+$ java -cp .:jcabi-github-0.23-jar-with-dependencies.jar com.chbrown13.pull_rec.PullRecommender
 ```
--- com/chbrown13/pull_rec/PullRecommender.java
+++ com/chbrown13/pull_rec/PullRecommender.java
@@ -5,8 +5,7 @@ import com.jcabi.http.response.JsonResponse;
 import java.io.*;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
-import javax.json.JsonObject;
-import org.apache.commons.mail.*;
+import javax.json.*;
 
 
 /**
@@ -14,193 +13,104 @@ import org.apache.commons.mail.*;
  */
 public class PullRecommender {
 
-	private Repo repo;
-	private Set<Integer> prs = new HashSet<Integer>();	
-	private int recs = 0;;
-	private int removed = 0;
-	private int fixes = 0;
+	private static int year = 2012;
 
-	public PullRecommender(Repo repo) {
-		this.repo = repo;
-		Utils.setProjectName(repo.coordinates().repo());
-		Utils.setProjectOwner(repo.coordinates().user());
-	}
-
-	/**
-	 * Sends email to researchers for review
-	 * 
-	 * @param text    Contents of email message
-	 * @param subject Subject of the email
-	 * @param pr	  Pull request number
-	 */
-	private static void sendEmail(String text, String subject, int pr) {
-		SimpleEmail email = new SimpleEmail();
-		String viewChanges = Comment.changes.replace("{user}", Utils.getProjectOwner())
-			.replace("{repo}", Utils.getProjectName())
-			.replace("{num}", Integer.toString(pr));
-		try {
-			email.setHostName("smtp.googlemail.com");
-			email.setSmtpPort(465);
-			email.setAuthenticator(new DefaultAuthenticator("toolrecommenderbot", "bot-recommender-tool"));
-			email.setSSLOnConnect(true);
-			email.setFrom("toolrecommenderbot@gmail.com");
-			email.setSubject("[tool-recommender-bot] " + subject);
-			email.setMsg(String.join("\n", viewChanges, text));
-			email.addTo("dcbrow10@ncsu.edu");
-			email.send();		
-			System.out.println("Email sent for review.");	
-		} catch (Exception e) {
-			e.printStackTrace();
+	private static String getDates(int month) {
+		String date = "{y1}-{m1}-01..{y2}-{m2}-01";
+		int m1 = month % 12;
+		int m2 = (month + 1) % 12;
+		if (m1 == 0) {
+			date = date.replace("{y1}", Integer.toString(year))
+				.replace("{y2}", Integer.toString(year+1))
+				.replace("{m1}", "12").replace("{m2}", "01");
+			year += 1;
+		} else if (m2 == 0) {
+			date = date.replace("{y1}", Integer.toString(year))
+				.replace("{y2}", Integer.toString(year))
+				.replace("{m1}", Integer.toString(m1))
+				.replace("{m2}", "12");
+		} else if (m2 < 10) {
+			date = date.replace("{y1}", Integer.toString(year))
+				.replace("{y2}", Integer.toString(year))
+				.replace("{m1}", "0"+Integer.toString(m1))
+				.replace("{m2}", "0"+Integer.toString(m2));
+		} else if (m2 == 10) {
+			date = date.replace("{y1}", Integer.toString(year))
+				.replace("{y2}", Integer.toString(year))
+				.replace("{m1}", "09")
+				.replace("{m2}", "10");
 		}
-	}
-	
-	/**
-	 * Post message recommending ErrorProne to Github on pull request fixing error.
-	 *
-	 * @param comment   Comment with recommendation
-	 * @param pull	 	Pull request to comment on
-	 * @param error     Error fixed in pull request
-	 */
-	private void makeRecommendation(Tool tool, Pull.Smart pull, Error error, int line, Set<Error> errors) {
-		try {
-			String base = pull.json().getJsonObject("base").getString("sha");
-			String head = pull.json().getJsonObject("head").getString("sha");			
-			String comment = error.generateComment(tool, errors, base);
-			String args = String.join(" ", Utils.getProjectOwner(), Utils.getProjectName(), 
-				Integer.toString(pull.number()), "\""+comment+"\"", head, error.getLocalFilePath(), 
-				Integer.toString(line)
-			);
-
-			String run = Comment.cmd.replace("{args}", args);
-			sendEmail(String.join("\n", Comment.compile, run), "Recommendation Review", pull.number());
-			recs += 1;
-			prs.add(pull.number());
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
+		else {
+			date = date.replace("{y1}", Integer.toString(year))
+				.replace("{y2}", Integer.toString(year))
+				.replace("{m1}", Integer.toString(m1))
+				.replace("{m2}", Integer.toString(m2));
 		}
-	}
-
-	/**
-	 * Checks if the change is actually a fix or not
-	 */
-	private boolean isFix(Set<Error> base, Set<Error> pull, Error error, List<String> files) {
-		boolean fileCheck = false;
-		for (String f: files) {
-			if (error.getFilePath().contains(f)) {
-				fileCheck = true;
-				break;
-			}
-		}
-		if (base.size() > 0 && fileCheck) {
-			return Utils.isFix(error);
-		}
-		return false;
-	}
-
-	/**
-	 * Analyze code of files in pull request and compare to master branch.
-	 *
-	 * @param pull   Current pull request
-	 * @return       Number of recommendations made
-	 */
-	private void analyze(Pull.Smart pull) {
-		System.out.println("Analyzing PR #" + Integer.toString(pull.number()) + "...");
-		Tool tool = new ErrorProne();		
-		String developer = "";
-		boolean pullRec = false;
-		List<String> javaFiles = new ArrayList<String>();
-		Iterator<JsonObject> files = null;
-		try {
-			files = pull.files().iterator();			
-		} catch (Exception e) {
-			e.printStackTrace();
-			return;
-		}
-		while (files.hasNext()) {
-			JsonObject f = files.next();
-			if (f.getString("filename").endsWith(".java") && f.getString("status").equals("modified")) {
-				javaFiles.add(f.getString("filename"));
-				pullRec = true;
-			}		
-		}
-		if (!pullRec) {
-			System.out.println("No java changes.");
-			return;
-		}
-		try {
-			Set<Error> baseErrors = Utils.checkout(pull, tool, true);
-			Set<Error> pullErrors = Utils.checkout(pull, tool, false);
-			if(baseErrors != null && pullErrors != null) {
-				System.out.println(Integer.toString(baseErrors.size()) +"------"+Integer.toString(pullErrors.size()));				
-				Set<Error> fixed = new HashSet<Error>();				
-				fixed.addAll(baseErrors);				
-				fixed.removeAll(pullErrors);
-				int i = 0;
-				for (Error e: fixed) {
-					if (isFix(baseErrors, pullErrors, e, javaFiles)) {
-						fixes += 1;						
-						System.out.println("Fixed "+ e.getKey() +" in PR #"+Integer.toString(pull.number())
-							+ " reported at line " + e.getLineNumberStr() + " possibly fixed at line " + Integer.toString(Utils.getFix()));
-						makeRecommendation(tool, pull, e, Utils.getFix(), baseErrors);
-					} else {
-						removed += 1;
-					}
-				}	
-			}
-			Utils.cleanup();
-		} catch (IOException e) {
-			e.printStackTrace();
-			Utils.cleanup();
-		}	
-	}
-
-	/**
-	 * Searches for new pull requests opened for a Github repository every 15 minutes.
-	 *
-	 * @return   List of new pull requests
-	 */
-	private ArrayList<Pull.Smart> getPullRequests() {
-		System.out.println("Getting new pull requests...");
-		ArrayList<Pull.Smart> requests = new ArrayList<Pull.Smart>();
-		Map<String, String> params = new HashMap<String, String>();
-		params.put("state", "open");
-		Iterator<Pull> pullit = this.repo.pulls().iterate(params).iterator();
-		int i = 0;
-		while (pullit.hasNext()) {
-			Pull.Smart pull = new Pull.Smart(pullit.next());
-			try {
-				if (new Date().getTime() - pull.createdAt().getTime() <= TimeUnit.MILLISECONDS.convert(15, TimeUnit.MINUTES)) {
-					analyze(pull);					
-					requests.add(pull);
-					String out = "Recommendations: {rec}\nFixes: {fix}\nRemoved: {rem}"
-						.replace("{rec}", Integer.toString(recs))
-						.replace("{fix}", Integer.toString(fixes - recs))
-						.replace("{rem}", Integer.toString(removed));
-					sendEmail(out, "Analyzed Pull Request", pull.number());						
-				} else {
-					System.out.println("No new pull requests");
-					break;
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-				return null;
-			}
-		}
-		return requests;
+		return date;
 	}
 
 	public static void main(String[] args) {
-		String[] acct = Utils.getCredentials(".github.creds");
-		RtGithub github = new RtGithub(acct[0], acct[1]);
-        Repo repo = github.repos().get(new Coordinates.Simple(args[0], args[1]));
-		PullRecommender recommender = new PullRecommender(repo);
-		ArrayList<Pull.Smart> requests = recommender.getPullRequests();
-		// System.out.println("{recs} recommendations made on {pulls} pull request(s) out of {totals} total. {fix} were just fixed."
-		// 	.replace("{recs}", Integer.toString(recs))
-		// 	.replace("{pulls}", Integer.toString(prs.size()))
-		// 	.replace("{totals}", Integer.toString(requests.size()))
-		// 	.replace("{fix}", Integer.toString(fixes - recs))
-		// );
+		int n = 0;
+		int x = 0;
+		int month = 1;
+		final Github github = new RtGithub("", "");
+		try { 
+			while (true) {
+				final JsonResponse resp = github.entry()
+					.uri().path("/search/repositories")
+					.queryParam("q", "language:java created:{date}".replace("{date}", getDates(month)))
+					.queryParam("sort", "forks")
+					.queryParam("order", "desc").back()
+					.fetch()
+					.as(JsonResponse.class);
+				month += 1;
+				JsonArray items = resp.json().readObject().getJsonArray("items");
+				if (items == null || items.isEmpty()) {
+					System.out.println(resp);
+					break;
+				} else {
+					final List<JsonObject> repos = items.getValuesAs(JsonObject.class);
+					for (final JsonObject r: repos) {
+						n += 1;
+						String coords = r.get("full_name").toString().replaceAll("\"", "");
+						Repo repo = github.repos().get(new Coordinates.Simple(coords));
+						// Map<String, String> params = new HashMap<String, String>();
+						// params.put("state", "open");
+						// Iterator<Pull> pulls = repo.pulls().iterate(params).iterator();
+						// int pr = 0;
+						// while (pr < 9 && pulls.hasNext()) {
+						// 	pulls.next();
+						// 	pr++;
+						// }
+						// if (!pulls.hasNext()) {
+						// 	continue;
+						// }
+						// Pull.Smart pull10 = new Pull.Smart(pulls.next());
+						// if (new Date().getTime() - pull10.createdAt().getTime() <= TimeUnit.MILLISECONDS.convert(180, TimeUnit.DAYS)
+						if (repo.contents().exists("pom.xml", "master")) {
+							x += 1;							
+							try {
+								PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("projects.txt", true)));
+								out.println(coords);
+								coords += "*";
+								out.close();
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						}
+						System.out.println(coords);				
+						if (n % 100 == 0) {
+							break;
+						}								
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(n);
+			System.out.println(x);
+		}
+		System.out.println(n);
+		System.out.println(x);
 	}
 }
