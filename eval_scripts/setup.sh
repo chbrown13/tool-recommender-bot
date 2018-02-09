88acf0ee10d5d1e9c2ff0d6aa00e0dd792fb2626
Chris Brown <chbrown13>
2018-02-09 11:05:00 -0500

    Removing extra setup cmds

--- eval_scripts/setup.sh
+++ eval_scripts/setup.sh
@@ -2,23 +2,6 @@
 sudo yum -y install epel-release
 sudo yum -y update
 
-# Install commands
-sudo yum -y install wget
-sudo yum -y install unzip
-sudo yum -y install python-pip
-sudo pip install --upgrade pip
-sudo pip install paramiko
-
-# Install Java
-sudo yum -y install java-1.8.0-openjdk.x86_64
-sudo cp /etc/profile /etc/profile_backup
-echo 'export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk' | sudo tee -a /etc/profile
-echo 'export JRE_HOME=/usr/lib/jvm/jre' | sudo tee -a /etc/profile
-source /etc/profile
-echo $JAVA_HOME
-echo $JRE_HOME
-cd ~
-
 # Install and Start Jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
@@ -33,17 +16,6 @@ wget http://localhost:8080/jnlpJars/jenkins-cli.jar
 wget http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz
 tar xvf apache-maven-3.0.5-bin.tar.gz
 sudo mv apache-maven-3.0.5  /usr/local/apache-maven
-export M2_HOME=/usr/local/apache-maven
-export M2=$M2_HOME/bin 
-export PATH=$M2:$PATH
-source ~/.bashrc
-echo "JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")" | sudo tee -a /etc/profile
-source /etc/profile
-
-# Install Groovy
-sudo yum -y install groovy
-
-# Create Jenkins user
 
 # Install Ansible
 sudo yum -y install ansible
@@ -55,5 +27,5 @@ wget https://raw.githubusercontent.com/chbrown13/PullRecommender/master/eval_scr
 wget https://gist.githubusercontent.com/chbrown13/ca345f0d96401144373b745975724412/raw/72cf4f75ec85bc9ad94c1e072965c797503e52b9/.projects_ssh
 wget https://raw.githubusercontent.com/chbrown13/PullRecommender/master/jenkins.xml
 wget https://raw.githubusercontent.com/chbrown13/PullRecommender/master/eval_scripts/eval.py
-python eval.py
-sudo ansible-playbook tool-recommender-bot.yml
+# python eval.py
+# sudo ansible-playbook tool-recommender-bot.yml
