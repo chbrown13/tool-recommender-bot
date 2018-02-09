d2716ec8f861a7d9c54a035a55bb6d5115baa346
Chris Brown <chbrown13>
2018-02-09 15:13:45 -0500

    No longer using paramiko ssh

--- eval_scripts/eval.py
+++ eval_scripts/eval.py
@@ -1,4 +1,4 @@
-import paramiko
+N = 100
 
 def jenkins(user, repo):
     with open("jenkins.xml") as f:
@@ -6,23 +6,10 @@ def jenkins(user, repo):
     config = config.replace("{user}", user).replace("{repo}", repo)
     return config
 
-def connection():
-    with open(".projects_ssh") as f:
-        connect = f.readlines()
-    return [c.replace("\n", "") for c in connect]
-
-def ssh():
-    ssh = paramiko.SSHClient()
-    ssh.load_system_host_keys()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    conn = connection()
-    ssh.connect(conn[0], username=conn[1], password=conn[2])
-    ssh_stdin, stdout, stderr = ssh.exec_command("python projects.py")
-    return stdout.readlines()
-
 def main():
     i = 0
-    projects = [x for x in ssh() if x != '\n']
+    with open("projects.txt") as f:
+        projects = f.readlines()
     for p in projects:
         git = p.replace("\n","").split("/")
         if git:
