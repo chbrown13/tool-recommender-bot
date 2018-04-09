09830098b80eb240be5298648f0d3cf22015eef9
Chris Brown <chbrown13>
2018-04-09 14:16:13 -0400

    Updates to setup script

--- eval_scripts/setup.sh
+++ eval_scripts/setup.sh
@@ -10,7 +10,6 @@ sudo systemctl start jenkins.service
 sudo systemctl enable jenkins.service
 sudo firewall-cmd --zone=public --permanent --add-port=8080/tcp
 sudo firewall-cmd --reload
-wget http://localhost:8080/jnlpJars/jenkins-cli.jar
 
 # Install Maven
 wget http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
@@ -24,8 +23,15 @@ sudo mv hosts /etc/ansible/hosts
 
 # Start tool-recommender-bot study
 wget https://raw.githubusercontent.com/chbrown13/PullRecommender/master/eval_scripts/tool-recommender-bot.yml
-wget https://gist.githubusercontent.com/chbrown13/ca345f0d96401144373b745975724412/raw/72cf4f75ec85bc9ad94c1e072965c797503e52b9/.projects_ssh
+wget https://raw.githubusercontent.com/chbrown13/tool-recommender-bot/master/eval_scripts/delete.yml
 wget https://raw.githubusercontent.com/chbrown13/PullRecommender/master/jenkins.xml
 wget https://raw.githubusercontent.com/chbrown13/PullRecommender/master/eval_scripts/eval.py
 python eval.py
-# sudo ansible-playbook tool-recommender-bot.yml
+
+## Manual Steps ##
+# Go to http://<ip>:8080/ 
+# Install suggested plugins
+# Follow instructions to start jenkins and create admin user
+# Enable anonymous authorization (Configure Global Security Settings, Anyone can do anything)
+# Disable requiretty in visudo
+# Change jenkins user privileges in visudo (jenkins  ALL= NOPASSWD: ALL)
