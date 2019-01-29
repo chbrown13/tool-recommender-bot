ab0e3fe9a16c537f8c4bf36667b853820fe8362e
Chris Brown <dcbrown06@gmail.com>
2019-01-29 09:49:12 -0500

    Fix spacing and continue if build succeeds

--- com/chbrown13/tool_rec/ErrorProne.java
+++ com/chbrown13/tool_rec/ErrorProne.java
@@ -12,50 +12,50 @@ import java.util.regex.*;
  */
 public class ErrorProne extends Tool {
 	
-	private final String MAVEN = "<plugin>\n"+
-	"<groupId>org.apache.maven.plugins</groupId>\n"+
-	"<artifactId>maven-compiler-plugin</artifactId>\n"+
-	"<version>3.5.1</version>\n"+
-	"<configuration>\n"+
-	"	<source>8</source>\n"+
-	"	<target>8</target>\n"+
-	"	<showWarnings>true</showWarnings>\n"+
-	"	<compilerArgs>\n"+
-	"		<arg>-XDcompilePolicy=simple</arg>\n"+
-	"		<arg>-Xplugin:ErrorProne -XepAllErrorsAsWarnings -Xep:AndroidJdkLibsChecker:ERROR -Xep:AssistedInjectAndInjectOnSameConstructor:ERROR -Xep:AutoFactoryAtInject:ERROR -Xep:ClassName:ERROR -Xep:ComparisonContractViolated:ERROR -Xep:DepAnn:ERROR -Xep:DivZero:ERROR -Xep:EmptyIf:ERROR -Xep:FuzzyEqualsShouldNotBeUsedInEqualsMethod:ERROR -Xep:InjectInvalidTargetingOnScopingAnnotation:ERROR -Xep:InjectMoreThanOneQualifier:ERROR -Xep:InjectScopeAnnotationOnInterfaceOrAbstractClass:ERROR -Xep:InjectScopeOrQualifierAnnotationRetention:ERROR -Xep:InjectedConstructorAnnotations:ERROR -Xep:InsecureCryptoUsage:ERROR -Xep:IterablePathParameter:ERROR -Xep:JMockTestWithoutRunWithOrRuleAnnotation:ERROR -Xep:Java7ApiChecker:ERROR -Xep:JavaxInjectOnFinalField:ERROR -Xep:LockMethodChecker:ERROR -Xep:LongLiteralLowerCaseSuffix:ERROR -Xep:NoAllocation:ERROR -Xep:NumericEquality:ERROR -Xep:ParameterPackage:ERROR -Xep:RestrictTo:ERROR -Xep:StaticOrDefaultInterfaceMethod:ERROR -Xep:UnlockMethod:ERROR -Xep:AnnotateFormatMethod:ERROR -Xep:AnnotationPosition:ERROR -Xep:AssertFalse:ERROR -Xep:AssistedInjectAndInjectOnConstructors:ERROR -Xep:BinderIdentityRestoredDangerously:ERROR -Xep:BindingToUnqualifiedCommonType:ERROR -Xep:ConstructorInvokesOverridable:ERROR -Xep:ConstructorLeaksThis:ERROR -Xep:EmptyTopLevelDeclaration:ERROR -Xep:EqualsBrokenForNull:ERROR -Xep:ExpectedExceptionChecker:ERROR -Xep:FunctionalInterfaceClash:ERROR -Xep:HardCodedSdCardPath:ERROR -Xep:InconsistentOverloads:ERROR -Xep:InvalidParam:ERROR -Xep:InvalidTag:ERROR -Xep:InvalidThrows:ERROR -Xep:MissingDefault:ERROR -Xep:MutableMethodReturnType:ERROR -Xep:NoFunctionalReturnType:ERROR -Xep:NonCanonicalStaticMemberImport:ERROR -Xep:NullableDereference:ERROR -Xep:PrimitiveArrayPassedToVarargsMethod:ERROR -Xep:ProtosAsKeyOfSetOrMap:ERROR -Xep:ProvidesFix:ERROR -Xep:QualifierWithTypeUse:ERROR -Xep:RedundantThrows:ERROR -Xep:ReturnFromVoid:ERROR -Xep:StaticQualifiedUsingExpression:ERROR -Xep:StringEquality:ERROR -Xep:SystemExitOutsideMain:ERROR -Xep:TestExceptionChecker:ERROR -Xep:UnnecessaryDefaultInEnumSwitch:ERROR -Xep:Unused:ERROR -Xep:UnusedException:ERROR -Xep:Var:ERROR -Xep:BooleanParameter:ERROR -Xep:ClassNamedLikeTypeParameter:ERROR -Xep:ConstantField:ERROR -Xep:EmptySetMultibindingContributions:ERROR -Xep:ExpectedExceptionRefactoring:ERROR -Xep:FieldCanBeFinal:ERROR -Xep:FieldMissingNullable:ERROR -Xep:ImmutableRefactoring:ERROR -Xep:LambdaFunctionalInterface:ERROR -Xep:MethodCanBeStatic:ERROR -Xep:MixedArrayDimensions:ERROR -Xep:MultiVariableDeclaration:ERROR -Xep:MultipleTopLevelClasses:ERROR -Xep:MultipleUnaryOperatorsInMethodCall:ERROR -Xep:PackageLocation:ERROR -Xep:ParameterComment:ERROR -Xep:ParameterNotNullable:ERROR -Xep:PrivateConstructorForNoninstantiableModule:ERROR -Xep:PrivateConstructorForUtilityClass:ERROR -Xep:RemoveUnusedImports:ERROR -Xep:ReturnMissingNullable:ERROR -Xep:ScopeOnModule:ERROR -Xep:SwitchDefault:ERROR -Xep:TestExceptionRefactoring:ERROR -Xep:ThrowsUncheckedException:ERROR -Xep:TryFailRefactoring:ERROR -Xep:TypeParameterNaming:ERROR -Xep:UngroupedOverloads:ERROR -Xep:UnnecessarySetDefault:ERROR -Xep:UnnecessaryStaticImport:ERROR -Xep:UseBinds:ERROR -Xep:WildcardImport:ERROR</arg>\n"+
-	"	</compilerArgs>\n"+
-	"	<annotationProcessorPaths>\n"+
-	"		<path>\n"+
-	"			<groupId>com.google.errorprone</groupId>\n"+
-	"			<artifactId>error_prone_core</artifactId>\n"+
-	"			<version>2.3.2</version>\n"+
-	"		</path>\n"+
-	"	</annotationProcessorPaths>\n"+
-	"</configuration>\n"+
-	"</plugin>\n";
+	private final String MAVEN = "{s}<plugin>\n"+
+	"{s}  <groupId>org.apache.maven.plugins</groupId>\n"+
+	"{s}  <artifactId>maven-compiler-plugin</artifactId>\n"+
+	"{s}  <version>3.5.1</version>\n"+
+	"{s}  <configuration>\n"+
+	"{s}    <source>8</source>\n"+
+	"{s}    <target>8</target>\n"+
+	"{s}    <showWarnings>true</showWarnings>\n"+
+	"{s}    <compilerArgs>\n"+
+	"{s}      <arg>-XDcompilePolicy=simple</arg>\n"+
+	"{s}      <arg>-Xplugin:ErrorProne -XepAllErrorsAsWarnings -Xep:AndroidJdkLibsChecker:ERROR -Xep:AssistedInjectAndInjectOnSameConstructor:ERROR -Xep:AutoFactoryAtInject:ERROR -Xep:ClassName:ERROR -Xep:ComparisonContractViolated:ERROR -Xep:DepAnn:ERROR -Xep:DivZero:ERROR -Xep:EmptyIf:ERROR -Xep:FuzzyEqualsShouldNotBeUsedInEqualsMethod:ERROR -Xep:InjectInvalidTargetingOnScopingAnnotation:ERROR -Xep:InjectMoreThanOneQualifier:ERROR -Xep:InjectScopeAnnotationOnInterfaceOrAbstractClass:ERROR -Xep:InjectScopeOrQualifierAnnotationRetention:ERROR -Xep:InjectedConstructorAnnotations:ERROR -Xep:InsecureCryptoUsage:ERROR -Xep:IterablePathParameter:ERROR -Xep:JMockTestWithoutRunWithOrRuleAnnotation:ERROR -Xep:Java7ApiChecker:ERROR -Xep:JavaxInjectOnFinalField:ERROR -Xep:LockMethodChecker:ERROR -Xep:LongLiteralLowerCaseSuffix:ERROR -Xep:NoAllocation:ERROR -Xep:NumericEquality:ERROR -Xep:ParameterPackage:ERROR -Xep:RestrictTo:ERROR -Xep:StaticOrDefaultInterfaceMethod:ERROR -Xep:UnlockMethod:ERROR -Xep:AnnotateFormatMethod:ERROR -Xep:AnnotationPosition:ERROR -Xep:AssertFalse:ERROR -Xep:AssistedInjectAndInjectOnConstructors:ERROR -Xep:BinderIdentityRestoredDangerously:ERROR -Xep:BindingToUnqualifiedCommonType:ERROR -Xep:ConstructorInvokesOverridable:ERROR -Xep:ConstructorLeaksThis:ERROR -Xep:EmptyTopLevelDeclaration:ERROR -Xep:EqualsBrokenForNull:ERROR -Xep:ExpectedExceptionChecker:ERROR -Xep:FunctionalInterfaceClash:ERROR -Xep:HardCodedSdCardPath:ERROR -Xep:InconsistentOverloads:ERROR -Xep:InvalidParam:ERROR -Xep:InvalidTag:ERROR -Xep:InvalidThrows:ERROR -Xep:MissingDefault:ERROR -Xep:MutableMethodReturnType:ERROR -Xep:NoFunctionalReturnType:ERROR -Xep:NonCanonicalStaticMemberImport:ERROR -Xep:NullableDereference:ERROR -Xep:PrimitiveArrayPassedToVarargsMethod:ERROR -Xep:ProtosAsKeyOfSetOrMap:ERROR -Xep:ProvidesFix:ERROR -Xep:QualifierWithTypeUse:ERROR -Xep:RedundantThrows:ERROR -Xep:ReturnFromVoid:ERROR -Xep:StaticQualifiedUsingExpression:ERROR -Xep:StringEquality:ERROR -Xep:SystemExitOutsideMain:ERROR -Xep:TestExceptionChecker:ERROR -Xep:UnnecessaryDefaultInEnumSwitch:ERROR -Xep:Unused:ERROR -Xep:UnusedException:ERROR -Xep:Var:ERROR -Xep:BooleanParameter:ERROR -Xep:ClassNamedLikeTypeParameter:ERROR -Xep:ConstantField:ERROR -Xep:EmptySetMultibindingContributions:ERROR -Xep:ExpectedExceptionRefactoring:ERROR -Xep:FieldCanBeFinal:ERROR -Xep:FieldMissingNullable:ERROR -Xep:ImmutableRefactoring:ERROR -Xep:LambdaFunctionalInterface:ERROR -Xep:MethodCanBeStatic:ERROR -Xep:MixedArrayDimensions:ERROR -Xep:MultiVariableDeclaration:ERROR -Xep:MultipleTopLevelClasses:ERROR -Xep:MultipleUnaryOperatorsInMethodCall:ERROR -Xep:PackageLocation:ERROR -Xep:ParameterComment:ERROR -Xep:ParameterNotNullable:ERROR -Xep:PrivateConstructorForNoninstantiableModule:ERROR -Xep:PrivateConstructorForUtilityClass:ERROR -Xep:RemoveUnusedImports:ERROR -Xep:ReturnMissingNullable:ERROR -Xep:ScopeOnModule:ERROR -Xep:SwitchDefault:ERROR -Xep:TestExceptionRefactoring:ERROR -Xep:ThrowsUncheckedException:ERROR -Xep:TryFailRefactoring:ERROR -Xep:TypeParameterNaming:ERROR -Xep:UngroupedOverloads:ERROR -Xep:UnnecessarySetDefault:ERROR -Xep:UnnecessaryStaticImport:ERROR -Xep:UseBinds:ERROR -Xep:WildcardImport:ERROR</arg>\n"+
+	"{s}    </compilerArgs>\n"+
+	"{s}    <annotationProcessorPaths>\n"+
+	"{s}      <path>\n"+
+	"{s}        <groupId>com.google.errorprone</groupId>\n"+
+	"{s}        <artifactId>error_prone_core</artifactId>\n"+
+	"{s}        <version>2.3.2</version>\n"+
+	"{s}      </path>\n"+
+	"{s}    </annotationProcessorPaths>\n"+
+	"{s}  </configuration>\n"+
+	"{s}</plugin>\n";
 
-	private final String PROFILE = "<profile>\n"+
-	"<id>jdk8</id>\n"+
-	"<activation>\n"+
-	"	<jdk>1.8</jdk>\n"+
-	"</activation>\n"+
-	"<build>\n"+
-	"	<plugins>\n"+
-	"		<plugin>\n"+
-	"			<groupId>org.apache.maven.plugins</groupId>\n"+
-	"			<artifactId>maven-compiler-plugin</artifactId>\n"+
-	"			<configuration>\n"+
-	"				<fork>true</fork>\n"+
-	"				<compilerArgs combine.children=\"append\">\n"+
-	"					<arg>-J-Xbootclasspath/p:${settings.localRepository}/com/google/errorprone/javac/${javac.version}/javac-${javac.version}.jar</arg>\n"+
-	"				</compilerArgs>\n"+
-	"			</configuration>\n"+
-	"		</plugin>\n"+
-	"	</plugins>\n"+
-	"</build>\n"+
-	"</profile>\n";
+	private final String PROFILE = "{s}<profile>\n"+
+	"{s}  <id>jdk8</id>\n"+
+	"{s}  <activation>\n"+
+	"{s}    <jdk>1.8</jdk>\n"+
+	"{s}  </activation>\n"+
+	"{s}  <build>\n"+
+	"{s}    <plugins>\n"+
+	"{s}      <plugin>\n"+
+	"{s}        <groupId>org.apache.maven.plugins</groupId>\n"+
+	"{s}        <artifactId>maven-compiler-plugin</artifactId>\n"+
+	"{s}        <configuration>\n"+
+	"{s}          <fork>true</fork>\n"+
+	"{s}          <compilerArgs combine.children=\"append\">\n"+
+	"{s}            <arg>-J-Xbootclasspath/p:${settings.localRepository}/com/google/errorprone/javac/${javac.version}/javac-${javac.version}.jar</arg>\n"+
+	"{s}          </compilerArgs>\n"+
+	"{s}        </configuration>\n"+
+	"{s}      </plugin>\n"+
+	"{s}    </plugins>\n"+
+	"{s}  </build>\n"+
+	"{s}</profile>\n";
 
-	private final String PROPERTY = "<javac.version>9+181-r4173-1</javac.version>\n";
+	private final String PROPERTY = "{s}<javac.version>9+181-r4173-1</javac.version>\n";
 
 	private final static String REC = "Looks like you're not using any error-checking in your Java build. This pull requests adds a static analysis tool, [Error Prone](http://errorprone.info), created by Google to find common errors in Java code. For example, running ```mvn compile``` on the following code:\n" +
 	"```java\n{code}\n```\n" +
@@ -104,10 +104,13 @@ public class ErrorProne extends Tool {
 	public List<Error> parseOutput(String log) {
 		List<Error> errors = new ArrayList<Error>();
 		if (log == null || log.isEmpty()) {
-			return errors;
+			return null;
 		} else if (log.contains("java.lang.RuntimeException")) {
 			System.out.println("Maven build error");
 			return null;
+		} else if (log.contains("[INFO] BUILD SUCCESS")) {
+			System.out.println("No errors.");
+			return errors;
 		}
 		String regex = "^[\\[ERROR\\]\\s]*/[(/\\w\\W)]+.java:\\[*\\d+(,|:)\\d+(:|\\])\\s(error:|warning:|\\[\\w+\\])";
 		Pattern pattern = Pattern.compile(regex);
